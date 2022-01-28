775187518b18a11ef8086196014fc6d067b9966c
Rodney Lafuente Mercado <mercrodn@gmail.com>
2022-01-28 16:31:19 -0500

    fix problem 1 based on ed post

--- HW1/T1_P1.py
+++ HW1/T1_P1.py
@@ -16,21 +16,19 @@ data = [(0., 0.),
         (6., 1.5),
         (8., 0.5)]
 
-def f(x, tau):
-    res = 0
-    for x_n, y_n in data:
-        # do not include K(x_n, x)y_n in sum when calculating f(x)
-        if not x_n == x:
-            res += math.exp( (- (x_n - x) ** 2) / tau) * y_n
-
-    return res
-
-
 def compute_loss(tau):
     loss = 0
 
     for x_i, y_i in data:
-        loss += (y_i - f(x_i, tau)) ** 2
+        f_i = 0
+
+        for x_n, y_n in data:
+            # do not include K(x_n, x)y_n in sum when calculating f(x)
+            # since we are calculating loss over training data
+            if not x_n == x_i:
+                f_i += math.exp( (- (x_n - x_i) ** 2) / tau) * y_n
+
+        loss += (y_i - f_i) ** 2
 
     return loss
 
@@ -38,11 +36,14 @@ for tau in (0.01, 2, 100):
     print("Loss for tau = " + str(tau) + ": " + str(compute_loss(tau)))
 
 
-
-
-
-
 # Make plots for problem 1.4
+def f(x, tau):
+    res = 0
+
+    for x_n, y_n in data:
+        res += math.exp( (- (x_n - x) ** 2) / tau) * y_n
+    
+    return res
 
 x = np.arange(0, 12, 0.1)
 
