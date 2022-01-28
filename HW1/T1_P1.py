1991a9434aff7bb20c989110c6ba66db56f25977
Rodney Lafuente Mercado <mercrodn@gmail.com>
2022-01-27 23:07:23 -0500

    complete problem 1.3

--- HW1/T1_P1.py
+++ HW1/T1_P1.py
@@ -5,6 +5,7 @@
 ##################
 
 import numpy as np
+import math
 
 data = [(0., 0.),
         (1., 0.5),
@@ -14,10 +15,23 @@ data = [(0., 0.),
         (6., 1.5),
         (8., 0.5)]
 
+
 def compute_loss(tau):
-    # TODO
     loss = 0
+
+    for x_i, y_i in data:
+        f_i = 0 
+
+        for x_n, y_n in data:
+            # Calculate f(x_i) without including K(x_i, x_i) in sum
+            if not x_n == x_i:
+                f_i += math.exp( (- (x_n - x_i) ** 2) / tau) * y_n
+
+        loss += (y_i - f_i) ** 2
+
     return loss
+
 
 for tau in (0.01, 2, 100):
     print("Loss for tau = " + str(tau) + ": " + str(compute_loss(tau)))
+
