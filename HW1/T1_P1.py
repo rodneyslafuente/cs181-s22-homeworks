092dfbf7f255219bac4f709dd017bfccad7e75fd
Rodney Lafuente Mercado <mercrodn@gmail.com>
2022-01-28 16:21:29 -0500

    add plot for problem 1.4

--- HW1/T1_P1.py
+++ HW1/T1_P1.py
@@ -4,7 +4,6 @@
 # STARTER CODE
 ##################
 
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import math
@@ -17,25 +16,44 @@ data = [(0., 0.),
         (6., 1.5),
         (8., 0.5)]
 
+def f(x, tau):
+    res = 0
+    for x_n, y_n in data:
+        # do not include K(x_n, x)y_n in sum when calculating f(x)
+        if not x_n == x:
+            res += math.exp( (- (x_n - x) ** 2) / tau) * y_n
+
+    return res
+
 
 def compute_loss(tau):
     loss = 0
 
     for x_i, y_i in data:
-        f_i = 0 
-
-        for x_n, y_n in data:
-            # Calculate f(x_i) without including K(x_i, x_i) in sum
-            if not x_n == x_i:
-                f_i += math.exp( (- (x_n - x_i) ** 2) / tau) * y_n
-
-        loss += (y_i - f_i) ** 2
+        loss += (y_i - f(x_i, tau)) ** 2
 
     return loss
-
 
 for tau in (0.01, 2, 100):
     print("Loss for tau = " + str(tau) + ": " + str(compute_loss(tau)))
 
 
+
+
+
+
 # Make plots for problem 1.4
+
+x = np.arange(0, 12, 0.1)
+
+plt.figure()
+
+for tau in (0.01, 2, 100):
+    plt.plot(x, [f(x_i, tau) for x_i in x], label=r'$\tau = $'+str(tau), linestyle="", marker="o")
+
+plt.xlabel(r'$x^*$')
+plt.ylabel(r'$f(x^*)$')
+plt.title(r"$f(x^*)$ with different lengthscales")
+plt.legend();
+
+plt.savefig('P1.png');
