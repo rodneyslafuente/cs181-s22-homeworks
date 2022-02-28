389dde99e593447a29761a0c8f9dd96dc81b1c89
Rodney Lafuente Mercado <mercrodn@gmail.com>
2022-02-27 23:00:44 -0500

    implement sigma fitting

--- HW2/T2_P3_GaussianGenerativeModel.py
+++ HW2/T2_P3_GaussianGenerativeModel.py
@@ -1,3 +1,4 @@
+from select import kevent
 import numpy as np
 import pandas as pd
 from scipy.stats import multivariate_normal as mvn  # you may find this useful
@@ -14,11 +15,15 @@ class GaussianGenerativeModel:
         self.is_shared_covariance = is_shared_covariance
         self.K = K
 
+    def __one_hot(self, y):
+        encoding = np.zeros(self.K)
+        encoding[y] = 1
+        return encoding.reshape(-1, 1)
+
     def __fitPi(self, X, y):
-        pi = []
         N = len(X)
 
-        # count number of occurrences of each class
+        # counts[k] = no. data points classified as C_k (scalar)
         counts = np.zeros(self.K)
         for i in range(N):
             counts[y[i]] += 1
@@ -26,14 +31,38 @@ class GaussianGenerativeModel:
         self.pi = np.array([counts[k] / N for k in range(self.K)])
 
     def __fitMu(self, X, y):
-        pass
+        N = len(X)
+
+        # counts[k] = no. data points classified as C_k (scalar)
+        # sums[k] = sum of x_i's classified as C_k (vector with same shape as x_i)
+        counts = np.zeros(self.K)
+        sums = np.zeros((self.K, X.shape[1]))
+        for i in range(N):
+            counts[y[i]] += 1
+            sums[y[i]] += X[i]
+
+        self.mu = [sums[k] / counts[k] for k in range(self.K)]
 
     def __fitSigma(self, X, y):
-        pass
+        N = len(X)
+        encoded_y = np.array([self.__one_hot(y_i) for y_i in y])
+        self.Sigma = np.zeros((X.shape[1], X.shape[1]))
 
+        for i in range(N):
+            for k in range(self.K):
+                term_to_add = encoded_y[i][k] * np.dot((X[i] - self.mu[k]).reshape(-1, 1), (X[i] - self.mu[k]).reshape(-1, 1).T)
+                assert(term_to_add.shape == self.Sigma.shape)
+                self.Sigma += term_to_add
+        
+        self.Sigma /= N
     # TODO: Implement this method!
     def fit(self, X, y):
-        return
+        encoded_y = np.array([self.__one_hot(y_i) for y_i in y])
+
+        self.__fitPi(X, y)
+        self.__fitMu(X, y)
+        self.__fitSigma(X, y)
+        return 
 
     # TODO: Implement this method!
     def predict(self, X_pred):
@@ -64,6 +93,8 @@ class GaussianGenerativeModel:
         y = np.array([star_labels[x] for x in df['Type']])
 
         self.__fitPi(X, y)
+        self.__fitMu(X, y)
+        self.__fitSigma(X, y)
 
 if __name__ == '__main__':
     model = GaussianGenerativeModel()
