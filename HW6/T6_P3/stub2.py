b2faf03eddf3bad6aeb0cb7380d0f42085745f4a
Rodney Lafuente Mercado <mercrodn@gmail.com>
2022-04-30 00:09:33 -0400

    finish pset

--- HW6/T6_P3/stub2.py
+++ HW6/T6_P3/stub2.py
@@ -26,16 +26,19 @@ class Learner(object):
         self.last_action = None
         self.last_reward = None
         self.is_first_state = True
+        self.is_second_state = False
 
         # We initialize our Q-value grid that has an entry for each action and state.
-        # (action, rel_x, rel_y)
-        self.Q = np.zeros((2, X_SCREEN // X_BINSIZE, Y_SCREEN // Y_BINSIZE))
+        # (action, rel_x, rel_y, gravity)
+        self.Q = np.zeros((2, X_SCREEN // X_BINSIZE, Y_SCREEN // Y_BINSIZE, 2))
 
     def reset(self):
         self.last_state = None
         self.last_action = None
         self.last_reward = None
         self.is_first_state = True
+        self.is_second_state = False
+        self.gravity = None
 
     def discretize_state(self, state):
         """
@@ -57,33 +60,53 @@ class Learner(object):
         # Do nothing if first state
         if self.is_first_state:
             self.is_first_state = False
+            self.is_second_state = True
             self.last_action = 0
             self.last_state = state
+            self.gravity = 1
             return self.last_action
 
+        if self.is_second_state:
+            self.is_second_state = False
+            if state["monkey"]["vel"] == -4:
+                self.gravity = 1
+            else:
+                self.gravity = 0
+
         # Define variables
-        alpha = 0.1
+        alpha_1 = 0.1
+        alpha_2 = 0.01
         epsilon = 0.001
         gamma = 0.9
+
+        other_gravity = None
+        if self.gravity == 0:
+            other_gravity = 1
+        else:
+            other_gravity = 0
 
         # 1. Discretize 'state' to get your transformed 'current state' features.
         last_x, last_y = self.discretize_state(self.last_state)
         curr_x, curr_y = self.discretize_state(state)
 
         # 2. Perform the Q-Learning update using 'current state' and the 'last state'.
-        self.Q[self.last_action, last_x, last_y] += alpha * \
-            (self.last_reward + gamma * np.amax(self.Q[:, curr_x, curr_y]) \
-                - self.Q[self.last_action, last_x, last_y])
+        self.Q[self.last_action, last_x, last_y, self.gravity] += alpha_1 * \
+            (self.last_reward + gamma * np.amax(self.Q[:, curr_x, curr_y, self.gravity]) \
+                - self.Q[self.last_action, last_x, last_y, self.gravity])
+        
+        self.Q[self.last_action, last_x, last_y, other_gravity] += alpha_2 * \
+            (self.last_reward + gamma * np.amax(self.Q[:, curr_x, curr_y, other_gravity]) \
+                - self.Q[self.last_action, last_x, last_y, other_gravity])
 
         # 3. Choose the next action using an epsilon-greedy policy.
         new_action = None
         if npr.rand() < epsilon:
-            if npr.rand() < 0.5: # 'explore' by jumping and doing nothing with equal prob.
+            if npr.rand() < 0.3:
                 new_action = 1
             else:
                 new_action = 0
         else:
-            new_action = np.argmax(self.Q[:, curr_x, curr_y])
+            new_action = np.argmax(self.Q[:, curr_x, curr_y, self.gravity])
 
         self.last_action = new_action
         self.last_state = state
@@ -129,8 +152,20 @@ if __name__ == '__main__':
     hist = []
 
     # Run games. You can update t_len to be smaller to run it faster.
-    run_games(agent, hist, 100, 1)
-    print(hist)
+    maxes = []
+    means = []
+    variances = []
+    for _ in range(10):
+        run_games(agent, hist, 100, 1)
+        maxes.append(np.amax(hist))
+        means.append(np.mean(hist))
+        variances.append(np.var(hist))
+
+    print('avg_max: '+str(np.mean(maxes)))
+    print('avg_mean: '+str(np.mean(means)))
+    print('overall std. dev.:'+str(np.sqrt(np.mean(variances))))
+
+    # print(hist)
 
     # Save history. 
     np.save('hist', np.array(hist))
--- HW6/T6_P3/test.py
+++ HW6/T6_P3/test.py
@@ -1,14 +1,19 @@
 import numpy as np
+import numpy.random as npr
 
 X_BINSIZE = 200
 Y_BINSIZE = 100
 X_SCREEN = 1400
 Y_SCREEN = 900
 
-Q = np.arange(2 * X_SCREEN // X_BINSIZE * Y_SCREEN // Y_BINSIZE)
-Q = np.reshape(Q, (2, X_SCREEN // X_BINSIZE, Y_SCREEN // Y_BINSIZE))
+# Q = np.arange(2 * X_SCREEN // X_BINSIZE * Y_SCREEN // Y_BINSIZE)
+# Q = np.reshape(Q, (2, X_SCREEN // X_BINSIZE, Y_SCREEN // Y_BINSIZE))
 
-state = (1, 1)
-rel_x, rel_y = state
+Q = np.zeros((2, X_SCREEN // X_BINSIZE, Y_SCREEN // Y_BINSIZE))
 
-print(np.argmax(Q[:, rel_x, rel_y]))
+# state = (1, 1)
+# rel_x, rel_y = state
+
+# print(np.argmax(Q[:, rel_x, rel_y]))
+
+print(Q.shape)
